af8f4c56107423b28b355d05e4e8a417c765909a
kennedy-whytech <kennedy.whytech@gmail.com>
2024-02-19 04:07:48 -0500

    just use remote backend for state

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-variable "kubeconfig_content" {
-  description = "The content of the kubeconfig file"
-  type        = string
-  sensitive   = true
-}
-
-resource "local_file" "kubeconfig" {
-  content  = var.kubeconfig_content
-  filename = "${path.module}/kubeconfig"
-}
-
 provider "kubernetes" {
-  config_path = local_file.kubeconfig.filename
+  config_path = var.kubeconfig_path
 }
